e224212f65c9403d017090c4fc4911b6d9311801
MathieuCliche <mc863@cornell.edu>
2014-06-17 23:39:27 -0400

    a msg

--- app/__init__.py
+++ app/__init__.py
@@ -8,8 +8,8 @@ app.secret_key = 'bacon'
 
 
 class Main(flask.views.MethodView):
-    def get(self, oldmessage=False):
-        return flask.render_template('index.html', oldmessage=oldmessage)
+    def get(self):
+        return flask.render_template('index.html')
 
     
 @app.route('/_compute')
@@ -27,7 +27,7 @@ class Contact(flask.views.MethodView):
     
 
 
-app.add_url_rule('/',view_func=Main.as_view('main'), methods=["GET","POST"])
+app.add_url_rule('/',view_func=Main.as_view('main'), methods=["GET"])
 app.add_url_rule('/about/',view_func=About.as_view('about'), methods=["GET"])
 app.add_url_rule('/contact/',view_func=Contact.as_view('contact'), methods=["GET"])
 
--- runp-heroku.py
+++ runp-heroku.py
@@ -1,1 +1,16 @@
-from app import app
+#from app import app
+
+import flask
+
+app = flask.Flask(__name__)
+
+
+
+class Main(flask.views.MethodView):
+    def get(self):
+        return "Hello world"
+
+app.add_url_rule('/',view_func=Main.as_view('main'), methods=["GET"])
+
+app.debug = False
+app.run()
