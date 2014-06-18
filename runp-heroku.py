50867058e391b393a7302e408b0277c6c87ba6fa
MathieuCliche <mc863@cornell.edu>
2014-06-17 23:49:45 -0400

    a msg

--- runp-heroku.py
+++ runp-heroku.py
@@ -1,16 +1,10 @@
 #from app import app
 
-import flask, flask.views
+import os
+from flask import Flask
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
 
-
-
-class Main(flask.views.MethodView):
-    def get(self):
-        return "Hello world"
-
-app.add_url_rule('/',view_func=Main.as_view('main'), methods=["GET"])
-
-app.debug = False
-app.run()
+@app.route('/')
+def hello():
+    return 'Hello World!'
