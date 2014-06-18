7d077b989603528091e3a4bcfb0a3b91b8130881
MathieuCliche <mc863@cornell.edu>
2014-06-17 23:13:29 -0400

    a msg

--- runp-heroku.py
+++ runp-heroku.py
@@ -7,5 +7,5 @@ from app import app
 #----------------------------------------
 
 if __name__ == "__main__":
-	port = int(os.environ.get("PORT", 5000))
-	app.run(host='0.0.0.0', port=port)
+	#port = int(os.environ.get("PORT", 5000))
+	app.run()#host='0.0.0.0', port=port)
