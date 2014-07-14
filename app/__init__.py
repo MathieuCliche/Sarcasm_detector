c31163f1e91f442653979723795f2f49e94ecd68
MathieuCliche <mc863@cornell.edu>
2014-07-14 10:59:57 -0400

    'mod'

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,8 @@
 import os
-#@app.route('/')
-#def hello():
-#    return 'Hello World!'
-
-
 import flask, flask.views
 from flask import Markup
 from flask import jsonify
-#import evaluate
+import evaluate
 
 app = flask.Flask(__name__)
 
@@ -28,10 +23,10 @@ app.add_url_rule('/',view_func=Main.as_view('main'), methods=["GET"])
 app.add_url_rule('/about/',view_func=About.as_view('about'), methods=["GET"])
 app.add_url_rule('/contact/',view_func=Contact.as_view('contact'), methods=["GET"])
 
-"""
+
 @app.route('/_compute')
 def compute():
     sentence = flask.request.args.get('sentence')
     percentage = evaluate.tweetscore(sentence)
     return jsonify(result=percentage)
-"""
+
