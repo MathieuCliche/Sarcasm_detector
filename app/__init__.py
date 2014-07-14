32b95c3cbbc22054e7ab4c33f2d985533fea46d9
MathieuCliche <mc863@cornell.edu>
2014-07-14 10:45:03 -0400

    'mod'

--- app/__init__.py
+++ app/__init__.py
@@ -1,24 +1,20 @@
 import os
-from flask import Flask
+#@app.route('/')
+#def hello():
+#    return 'Hello World!'
 
-app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return 'Hello World!'
-
-"""
 import flask, flask.views
 from flask import Markup
 from flask import jsonify
-import evaluate
+#import evaluate
 
 app = flask.Flask(__name__)
 
 class Main(flask.views.MethodView):
     def get(self):
-        return flask.render_template('index.html')
-    
+        return "Hello world" #flask.render_template('index.html')
+"""    
 class About(flask.views.MethodView):
     def get(self):
         return flask.render_template('about.html')
@@ -26,12 +22,12 @@ class About(flask.views.MethodView):
 class Contact(flask.views.MethodView):
     def get(self):
         return flask.render_template('contact.html')   
-
+"""
 
 app.add_url_rule('/',view_func=Main.as_view('main'), methods=["GET"])
-app.add_url_rule('/about/',view_func=About.as_view('about'), methods=["GET"])
-app.add_url_rule('/contact/',view_func=Contact.as_view('contact'), methods=["GET"])
-
+#app.add_url_rule('/about/',view_func=About.as_view('about'), methods=["GET"])
+#app.add_url_rule('/contact/',view_func=Contact.as_view('contact'), methods=["GET"])
+"""
 @app.route('/_compute')
 def compute():
     sentence = flask.request.args.get('sentence')
