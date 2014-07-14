590976a92853e5cdacf4f2f34c44670874db7f7b
MathieuCliche <mc863@cornell.edu>
2014-07-14 18:17:37 -0400

    'python_everywhere'

--- app/__init__.py
+++ app/__init__.py
@@ -3,16 +3,15 @@ import os
 import flask, flask.views
 from flask import Markup
 from flask import jsonify
-#import evaluate
+import evaluate
 
-application = flask.Flask(__name__)
+app = flask.Flask(__name__)
 
 class Main(flask.views.MethodView):
     def get(self):
         return flask.render_template('index.html')
     
-"""
-   
+ 
 class About(flask.views.MethodView):
     def get(self):
         return flask.render_template('about.html')
@@ -21,18 +20,14 @@ class Contact(flask.views.MethodView):
     def get(self):
         return flask.render_template('contact.html')   
 
-"""
-application.add_url_rule('/',view_func=Main.as_view('main'), methods=["GET"])
-"""
-application.add_url_rule('/about/',view_func=About.as_view('about'), methods=["GET"])
-application.add_url_rule('/contact/',view_func=Contact.as_view('contact'), methods=["GET"])
+app.add_url_rule('/',view_func=Main.as_view('main'), methods=["GET"])
+app.add_url_rule('/about/',view_func=About.as_view('about'), methods=["GET"])
+app.add_url_rule('/contact/',view_func=Contact.as_view('contact'), methods=["GET"])
 
 
-@application.route('/_compute')
+@app.route('/_compute')
 def compute():
     sentence = flask.request.args.get('sentence')
     percentage = evaluate.tweetscore(sentence)
     return jsonify(result=percentage)
     
-    """
-
