f785b1348a2dc337a538384655fb6d4e27acb5a5
MathieuCliche <mc863@cornell.edu>
2014-07-14 17:01:35 -0400

    'aws'

--- app/__init__.py
+++ app/__init__.py
@@ -3,13 +3,15 @@ import os
 import flask, flask.views
 from flask import Markup
 from flask import jsonify
-import evaluate
+#import evaluate
 
 application = flask.Flask(__name__)
 
 class Main(flask.views.MethodView):
     def get(self):
-        return flask.render_template('index.html')
+        return "Hello world"#flask.render_template('index.html')
+    
+"""
    
 class About(flask.views.MethodView):
     def get(self):
@@ -19,8 +21,9 @@ class Contact(flask.views.MethodView):
     def get(self):
         return flask.render_template('contact.html')   
 
-
+"""
 application.add_url_rule('/',view_func=Main.as_view('main'), methods=["GET"])
+"""
 application.add_url_rule('/about/',view_func=About.as_view('about'), methods=["GET"])
 application.add_url_rule('/contact/',view_func=Contact.as_view('contact'), methods=["GET"])
 
@@ -30,4 +33,6 @@ def compute():
     sentence = flask.request.args.get('sentence')
     percentage = evaluate.tweetscore(sentence)
     return jsonify(result=percentage)
+    
+    """
 
