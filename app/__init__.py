4782bfdd9fa636a5b069b9080f48fb437f56f5cc
MathieuCliche <mc863@cornell.edu>
2014-07-14 16:54:33 -0400

    'aws'

--- app/__init__.py
+++ app/__init__.py
@@ -1,26 +1,11 @@
-import flask
- 
-application = flask.Flask(__name__)
 
-#Set application.debug=true to enable tracebacks on Beanstalk log output. 
-#Make sure to remove this line before deploying to production.
-application.debug=True
- 
-@application.route('/')
-def hello_world():
-    return "Hello world!"
- 
-if __name__ == '__main__':
-    application.run(host='0.0.0.0', debug=True)
-
-"""
 import os
 import flask, flask.views
 from flask import Markup
 from flask import jsonify
 import evaluate
 
-app = flask.Flask(__name__)
+application = flask.Flask(__name__)
 
 class Main(flask.views.MethodView):
     def get(self):
@@ -35,15 +20,14 @@ class Contact(flask.views.MethodView):
         return flask.render_template('contact.html')   
 
 
-app.add_url_rule('/',view_func=Main.as_view('main'), methods=["GET"])
-app.add_url_rule('/about/',view_func=About.as_view('about'), methods=["GET"])
-app.add_url_rule('/contact/',view_func=Contact.as_view('contact'), methods=["GET"])
+application.add_url_rule('/',view_func=Main.as_view('main'), methods=["GET"])
+application.add_url_rule('/about/',view_func=About.as_view('about'), methods=["GET"])
+application.add_url_rule('/contact/',view_func=Contact.as_view('contact'), methods=["GET"])
 
 
-@app.route('/_compute')
+@application.route('/_compute')
 def compute():
     sentence = flask.request.args.get('sentence')
     percentage = evaluate.tweetscore(sentence)
     return jsonify(result=percentage)
 
-"""
--- application.py
+++ application.py
@@ -1,3 +1,8 @@
+from app import application
+
+
+
+""""
 import flask
  
 application = flask.Flask(__name__)
@@ -12,3 +17,5 @@ def hello_world():
  
 if __name__ == '__main__':
     application.run(host='0.0.0.0', debug=True)
+    
+    """""
